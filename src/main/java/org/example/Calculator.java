3c82482d3490b73309a21d32d8e685bb6e627549
Adilson Fuxe <adilson.fuxe@bancobai.ao>
2024-10-25 01:55:01 +0100

    test: create first unit test method

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Calculator {
+  public int integerDivision(int dividend, int divisor) {
+    return dividend/divisor;
+  }
+}
---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+  @Test
+  void integerDivision() {
+    Calculator calculator = new Calculator();
+    int result = calculator.integerDivision(4, 2);
+    Assertions.assertEquals(2, result);
+  }
+}
