9ba5dcb5c65efec9b13289e953c1d7c7879c5a7f
Adilson Fuxe <adilson.fuxe@bancobai.ao>
2024-10-28 08:30:06 +0100

    test: assert an exception

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -2,18 +2,37 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 
 @DisplayName("Test Math operator in calculator class")
 public class CalculatorTest {
+  Calculator calculator;
+  @BeforeAll
+  static void  setup(){
+    System.out.println("Executing @BeforeAll  method");
+  }
+
+  @AfterAll
+  static void cleanup() {
+    System.out.println("Executing  @AfterAll   method");
+  }
+
+  @BeforeEach
+  void beforeEachTestMethod(){
+    calculator = new Calculator();
+    System.out.println("Executing  @BeforeEach  method");
+  }
+
+  @AfterEach
+  void afterEachTestMethod(){
+    System.out.println("Executing  @AfterEach  method");
+  }
 
   @DisplayName("Test 4/2")
   @Test
   void testIntegerDivision_WhenFourIsDividedByTwo_ShouldReturnTwo() {
     // Arrange // Given
-    Calculator calculator = new Calculator();
     int dividend = 4;
     int divisor = 2;
     int expectedResult = 2;
@@ -25,16 +44,23 @@ public class CalculatorTest {
     assertEquals(expectedResult, result, ()-> "4/2 did not produce 2");
   }
 
+  //@Disabled("TODO: Still need to work on it")
   @DisplayName("Division by zero")
   @Test
   void testIntegerDivision_WhenDividendIsDividedByZero_ShouldThrownArithmeticException(){
-    fail("Not implemented yet");
+    // Arrange // Given
+    int dividend = 4;
+    int divisor = 0;
+
+    // Acct and Assert
+    assertThrows(ArithmeticException.class, ()-> {
+      calculator.integerDivision(dividend, divisor);
+    });
   }
 
   @DisplayName("Test 5-2")
   @Test
   void testIntegerSubtraction_WhenFiveSubtrahendByTwo_ShouldReturnThree(){
-    Calculator calculator = new Calculator();
     int result = calculator.integerSubtraction(5,2);
     assertEquals(3, result, () -> "5-2 did not produce 3");
   }
